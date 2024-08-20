6d354b598ad2b906280b912971a527ce199c3415
vaibhav-saxena25 <149486002+vaibhav-saxena25@users.noreply.github.com>
2024-08-20 17:37:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const options = {
         },
         servers:[
             {
-                url:"http://localhost:8080"
+                //url:"http://localhost:8080"
+                url:"https://job-portal-app-backend.onrender.com/api-doc/"
             }
         ]
     },
